02ec3e02ccca5e8bf8a1f061e9bbf153192144db
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2012-12-28 05:05:27 +0200

    scss mode

--- init.el
+++ init.el
@@ -90,7 +90,8 @@
 (setq my-packages (append '(el-get ack yasnippet ruby-compilation Enhanced-Ruby-Mode color-theme-solarized
 				   auto-complete auto-complete-emacs-lisp auto-complete-yasnippet
 				   anything anything-rcodetools emacs-w3m yaml-mode windcycle
-				   go-mode coffee-mode markdown-mode less-css-mode mustache-mode
+				   go-mode 
+				   coffee-mode markdown-mode less-css-mode scss-mode mustache-mode
 				   clojure-mode nrepl paredit 
 				   epresent org-html5presentation org-impress-js
 				   ) 

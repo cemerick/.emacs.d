b3973ec75ee720b61cced0b8e50c8153ba64e603
Daniel Szmulewicz <danielsz@sdf.lonestar.org>
2013-12-13 23:14:50 +0200

    Cask + cider

--- init.el
+++ init.el
@@ -1,8 +1,17 @@
 ;;; init.el --- Where all the magic begins
-;;
+
+;; Cask initialization
+
+  (let 
+      ((cask-file "~/.cask/cask.el")
+       (cask-spec "~/.emacs.d/Cask"))
+    (when (and (file-exists-p cask-file) (file-exists-p cask-spec))
+      (require 'cask cask-file)
+      (cask-initialize)
+      (require 'pallet)))
+     
 ;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
 ;; embedded in literate Org-mode files.
-
 
 (require 'org-install)
 (require 'ob-tangle)
